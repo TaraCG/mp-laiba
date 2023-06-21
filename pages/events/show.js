df05cf2454fb74e37b46742e92759f20d2660d70
nastyam1994 <nastyam_94@bk.ru>
2023-06-21 21:55:41 +0800

    Changed the main color. Added functionality to a banner on Event page

--- pages/events/show.js
+++ pages/events/show.js
@@ -18,6 +18,15 @@ Page({
       });
     }
   },
+
+  openFullImage: function () {
+    const imageUrl = 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80';
+
+    wx.previewImage({
+      urls: [imageUrl], 
+      current: imageUrl, 
+    });
+  },
   
   /**
    * Lifecycle function--Called when page load
